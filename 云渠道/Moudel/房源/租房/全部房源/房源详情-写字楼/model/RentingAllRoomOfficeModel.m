b77f59de2146ef96e4684fec5edce0a330f3df2c
xiaoginzq <122574357@QQ.COM>
2018-12-19 13:41:30 +0800

    脑壳痛

---
+++ 云渠道/Moudel/房源/租房/全部房源/房源详情-写字楼/model/RentingAllRoomOfficeModel.m
@@ -0,0 +1,13 @@
+//
+//  RentingAllRoomOfficeModel.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/12/19.
+//  Copyright © 2018 xiaoq. All rights reserved.
+//
+
+#import "RentingAllRoomOfficeModel.h"
+
+@implementation RentingAllRoomOfficeModel
+
+@end
---
+++ 云渠道/Moudel/房源/租房/全部房源/房源详情-商铺/model/RentingAllRoomStoreModel.m
@@ -0,0 +1,13 @@
+//
+//  RentingAllRoomStoreModel.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/12/19.
+//  Copyright © 2018 xiaoq. All rights reserved.
+//
+
+#import "RentingAllRoomStoreModel.h"
+
+@implementation RentingAllRoomStoreModel
+
+@end
