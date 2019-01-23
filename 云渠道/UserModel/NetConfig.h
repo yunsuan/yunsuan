cd7387a3be55ebbee5e6b3d205ed280c5e0bc962
xiaoginzq <122574357@QQ.COM>
2019-01-23 11:12:10 +0800

    修改

--- 云渠道/UserModel/NetConfig.h
+++ 云渠道/UserModel/NetConfig.h
@@ -16,7 +16,7 @@
 //static NSString *const Base_Net = @"http://120.27.21.136:2798/";
 
 //基础接口正式
-static NSString *const TestBase_Net = @"http://120.78.69.178:2902/";
+//static NSString *const TestBase_Net = @"http://120.78.69.178:2902/";
 
 //基础接口测试
 //static NSString *const TestBase_Net = @"http://120.27.21.136:2798/";
