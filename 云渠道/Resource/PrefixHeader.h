9a6ac312a6850f5329993c20fa0ee3c19e85518a
xiaoginzq <122574357@QQ.COM>
2019-02-01 10:15:59 +0800

    xiugai

--- 云渠道/Resource/PrefixHeader.h
+++ 云渠道/Resource/PrefixHeader.h
@@ -30,13 +30,13 @@
 #import "TransmitView.h"
 
 
-#define TestBase_Net  [[[NSMutableArray alloc]initWithContentsOfFile:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0]stringByAppendingPathComponent:@"ServerControl.plist"]] objectAtIndex:0]
+//#define TestBase_Net  [[[NSMutableArray alloc]initWithContentsOfFile:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0]stringByAppendingPathComponent:@"ServerControl.plist"]] objectAtIndex:0]
 
 
 /**
  *  全局
  */
-#define YQDversion @"3.2"
+#define YQDversion @"3.3"
 #define LOGINENTIFIER @"logIndentifier"
 #define LOGINSUCCESS @"logInSuccessdentifier"
 #define redirectUrl @"http://www.ccsoft.com.cn/"
