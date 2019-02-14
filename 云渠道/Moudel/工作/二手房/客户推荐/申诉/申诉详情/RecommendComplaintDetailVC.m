07970cf760846ece01fd8b38d84aa5edea5ffd7f
xiaoginzq <122574357@QQ.COM>
2019-02-14 18:33:02 +0800

    修改

--- 云渠道/Moudel/工作/二手房/客户推荐/申诉/申诉详情/RecommendComplaintDetailVC.m
+++ 云渠道/Moudel/工作/二手房/客户推荐/申诉/申诉详情/RecommendComplaintDetailVC.m
@@ -75,10 +75,10 @@
     if ([data[@"appeal_state"] isEqualToString:@"处理中"]) {
 //
 //        NSDictionary *dic = data[@"sub_info"];
-        _contentArr = @[@[[NSString stringWithFormat:@"推荐编号：%@",data[@"recommend_code"]],[NSString stringWithFormat:@"客户姓名：%@",data[@"client_name"]],[NSString stringWithFormat:@"客户性别：%@",[data[@"client_sex"] integerValue] == 1? @"男":@"女"],[NSString stringWithFormat:@"联系方式：%@",data[@"client_tel"]],[NSString stringWithFormat:@"推荐时间：%@",data[@"recommend_time"]],[NSString stringWithFormat:@"备注：%@",data[@"comment"]]],@[[NSString stringWithFormat:@"经纪人：%@",data[@"butter_name"]],[NSString stringWithFormat:@"联系电话：%@",data[@"butter_tel"]],[NSString stringWithFormat:@"门店编号：%@",data[@"store_code"]],[NSString stringWithFormat:@"门店名称：%@",data[@"store_name"]],[NSString stringWithFormat:@"接单时间：%@",data[@"accept_time"]]],@[[NSString stringWithFormat:@"失效时间：%@",data[@"disabled_time"]],[NSString stringWithFormat:@"失效类型：%@",data[@"disabled_state"]],[NSString stringWithFormat:@"失效原由：%@",data[@"disabled_reason"]]],@[[NSString stringWithFormat:@"申诉时间：%@",data[@"appeal_time"]],[NSString stringWithFormat:@"申诉类型：%@",data[@"appeal_type"]],[NSString stringWithFormat:@"申诉原由：%@",data[@"appeal_comment"]]]];
+        _contentArr = @[@[[NSString stringWithFormat:@"申诉时间：%@",data[@"appeal_time"]],[NSString stringWithFormat:@"申诉类型：%@",data[@"appeal_type"]],[NSString stringWithFormat:@"申诉原由：%@",data[@"appeal_comment"]]],@[[NSString stringWithFormat:@"失效时间：%@",data[@"disabled_time"]],[NSString stringWithFormat:@"失效类型：%@",data[@"disabled_state"]],[NSString stringWithFormat:@"失效原由：%@",data[@"disabled_reason"]]],@[[NSString stringWithFormat:@"经纪人：%@",data[@"butter_name"]],[NSString stringWithFormat:@"联系电话：%@",data[@"butter_tel"]],[NSString stringWithFormat:@"门店编号：%@",data[@"store_code"]],[NSString stringWithFormat:@"门店名称：%@",data[@"store_name"]],[NSString stringWithFormat:@"接单时间：%@",data[@"accept_time"]]],@[[NSString stringWithFormat:@"推荐编号：%@",data[@"recommend_code"]],[NSString stringWithFormat:@"客户姓名：%@",data[@"client_name"]],[NSString stringWithFormat:@"客户性别：%@",[data[@"client_sex"] integerValue] == 1? @"男":@"女"],[NSString stringWithFormat:@"联系方式：%@",data[@"client_tel"]],[NSString stringWithFormat:@"推荐时间：%@",data[@"recommend_time"]],[NSString stringWithFormat:@"备注：%@",data[@"comment"]]]];
     }else{
 //
-        _contentArr = @[@[[NSString stringWithFormat:@"处理人员：%@",data[@""]],[NSString stringWithFormat:@"处理时间：%@",data[@""]]],@[[NSString stringWithFormat:@"推荐编号：%@",data[@"recommend_code"]],[NSString stringWithFormat:@"客户姓名：%@",data[@"client_name"]],[NSString stringWithFormat:@"客户性别：%@",[data[@"client_sex"] integerValue] == 1? @"男":@"女"],[NSString stringWithFormat:@"联系方式：%@",data[@"client_tel"]],[NSString stringWithFormat:@"推荐时间：%@",data[@"recommend_time"]],[NSString stringWithFormat:@"备注：%@",data[@"comment"]]],@[[NSString stringWithFormat:@"经纪人：%@",data[@"butter_name"]],[NSString stringWithFormat:@"联系电话：%@",data[@"butter_tel"]],[NSString stringWithFormat:@"门店编号：%@",data[@"store_code"]],[NSString stringWithFormat:@"门店名称：%@",data[@"store_name"]],[NSString stringWithFormat:@"接单时间：%@",data[@"accept_time"]]],@[[NSString stringWithFormat:@"失效时间：%@",data[@"disabled_time"]],[NSString stringWithFormat:@"失效类型：%@",data[@"disabled_state"]],[NSString stringWithFormat:@"失效原由：%@",data[@"disabled_reason"]]],@[[NSString stringWithFormat:@"申诉时间：%@",data[@"appeal_time"]],[NSString stringWithFormat:@"申诉类型：%@",data[@"appeal_type"]],[NSString stringWithFormat:@"申诉原由：%@",data[@"appeal_comment"]]]];
+        _contentArr = @[@[[NSString stringWithFormat:@"处理人员：%@",data[@""]],[NSString stringWithFormat:@"处理时间：%@",data[@""]]],@[[NSString stringWithFormat:@"申诉时间：%@",data[@"appeal_time"]],[NSString stringWithFormat:@"申诉类型：%@",data[@"appeal_type"]],[NSString stringWithFormat:@"申诉原由：%@",data[@"appeal_comment"]]],@[[NSString stringWithFormat:@"失效时间：%@",data[@"disabled_time"]],[NSString stringWithFormat:@"失效类型：%@",data[@"disabled_state"]],[NSString stringWithFormat:@"失效原由：%@",data[@"disabled_reason"]]],@[[NSString stringWithFormat:@"经纪人：%@",data[@"butter_name"]],[NSString stringWithFormat:@"联系电话：%@",data[@"butter_tel"]],[NSString stringWithFormat:@"门店编号：%@",data[@"store_code"]],[NSString stringWithFormat:@"门店名称：%@",data[@"store_name"]],[NSString stringWithFormat:@"接单时间：%@",data[@"accept_time"]]],@[[NSString stringWithFormat:@"推荐编号：%@",data[@"recommend_code"]],[NSString stringWithFormat:@"客户姓名：%@",data[@"client_name"]],[NSString stringWithFormat:@"客户性别：%@",[data[@"client_sex"] integerValue] == 1? @"男":@"女"],[NSString stringWithFormat:@"联系方式：%@",data[@"client_tel"]],[NSString stringWithFormat:@"推荐时间：%@",data[@"recommend_time"]],[NSString stringWithFormat:@"备注：%@",data[@"comment"]]]];
     }
     [_detailTable reloadData];
 }
@@ -113,16 +113,16 @@
         
         if (section == 0) {
             
-            header.titleL.text = @"推荐信息";
+            header.titleL.text = @"申诉信息";
         }else if(section == 1){
             
-            header.titleL.text = @"接单信息";
+            header.titleL.text = @"失效信息";
         }else if(section == 2){
             
-            header.titleL.text = @"失效信息";
+            header.titleL.text = @"接单信息";
         }else if(section == 3){
             
-            header.titleL.text = @"申诉信息";
+            header.titleL.text = @"推荐信息";
         }else{
             
             header.titleL.text = @"申诉信息";
@@ -134,16 +134,16 @@
             header.titleL.text = @"处理信息";
         }else if(section == 1){
             
-            header.titleL.text = @"推荐信息";
+            header.titleL.text = @"申诉信息";
         }else if(section == 2){
             
-            header.titleL.text = @"接单信息";
+            header.titleL.text = @"失效信息";
         }else if(section == 3){
             
-            header.titleL.text = @"失效信息";
+            header.titleL.text = @"接单信息";
         }else{
             
-            header.titleL.text = @"申诉信息";
+            header.titleL.text = @"推荐信息";
         }
     }
     
