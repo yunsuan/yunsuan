35e173367a3e207c6e5b8bc72b8f179023dba28c
xiaoginzq <122574357@QQ.COM>
2019-02-19 16:06:25 +0800

    添加经办人

---
+++ 云渠道/Moudel/工作/二手房/合同签订/合同详情/添加经办人/ContractAddAgentVC.h
@@ -0,0 +1,17 @@
+//
+//  ContractAddAgentVC.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2019/2/19.
+//  Copyright © 2019 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContractAddAgentVC : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 云渠道/Moudel/工作/二手房/合同签订/合同详情/添加经办人/ContractAddAgentVC.m
@@ -0,0 +1,32 @@
+//
+//  ContractAddAgentVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2019/2/19.
+//  Copyright © 2019 xiaoq. All rights reserved.
+//
+
+#import "ContractAddAgentVC.h"
+
+@interface ContractAddAgentVC ()
+
+@end
+
+@implementation ContractAddAgentVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
