6ef29bff6631973b156f64af5efb7b8c24367274
xiaoginzq <122574357@QQ.COM>
2019-04-12 16:00:48 +0800

    修改401弹出

--- 云渠道/Other/BaseRequest.m
+++ 云渠道/Other/BaseRequest.m
@@ -158,8 +158,27 @@ static NSString *const kACCESSROLE = @"agent";
         
     } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         [WaitAnimation stopAnimation];
-        success(responseObject);
+//        success(responseObject);
         
+        if ([responseObject[@"code"] integerValue] == 200 || [responseObject[@"code"] integerValue] == 400)
+        {
+            success(responseObject);
+            return ;
+            
+        }else if ([responseObject[@"code"] integerValue] == 401) {
+            [BaseRequest showConten:@"账号在其他地点登录，请重新登录！"];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [[NSUserDefaults standardUserDefaults] removeObjectForKey:LOGINENTIFIER];
+                [UserModel defaultModel].Token = @"";
+                [UserModelArchiver archive];
+                [[NSNotificationCenter defaultCenter]postNotificationName:@"goLoginVC" object:nil];
+            });
+            return;
+        }else{
+            
+            [BaseRequest showConten:responseObject[@"msg"]];
+            return;
+        }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         [WaitAnimation stopAnimation];
         failure(error);
